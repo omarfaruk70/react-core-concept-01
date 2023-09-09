a6a72855238f75b8cea45af7dd5bf427ab4964b1
omarfaruk70 <faruk1611281892@gmail.com>
2023-09-10 01:02:57 +0600

    react css done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
@@ -14,6 +14,8 @@ function App() {
       <ShowName></ShowName>
       {/* another component function */}
       <NationAnthem></NationAnthem>
+      {/* another component function called */}
+      <FavouriteSub></FavouriteSub>
 
     </>
   )
@@ -25,12 +27,34 @@ function ShowName(){
 }
 // create another dynamic component function
 function NationAnthem(){
+  // add css styling with ClassName property form app.css imported file (ysing ClassName)
   return (
-   <div>
+   <div className='NationAnthem'>
     <h1>Amar sonar bangla ami tomay valobashi</h1>
     <p>cirodin tomar akash tomar batash amar praneee....</p>
     <small>O ma amar prane bajay bashi.</small>
+    <a href=""></a>
   </div>
+  )
+}
+
+// create another component 
+function FavouriteSub(){
+  // creat an object for css styling (using style={})
+  const DeveloperStyles = {
+    margin: '20px',
+    padding: '60px',
+    border: '5px solid purple',
+    borderRadius: '10px'
+  }
+  return(
+    <div style={DeveloperStyles}>
+      <h1>I have some knowledge about Physics, chemistry and Biologoy</h1>
+      <h2>I am intermediate with Bangla, English and Math</h2>
+      <p>
+        But I love to learn Programming
+      </p>
+    </div>
   )
 }
 
